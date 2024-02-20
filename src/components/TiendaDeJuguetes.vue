30ab202884d3d5865bded17e7da5295765476ea4
Joksan Bahena <20203tn008@utez.edu.mx>
2024-02-19 19:28:48 -0600

    captcha

--- src/components/TiendaDeJuguetes.vue
+++ src/components/TiendaDeJuguetes.vue
@@ -7,14 +7,39 @@
       href="http://localhost:5173/#/tienda-de-juguetes/juegos-electronicos"
       >Juegos electronicos</b-button
     >
-    <b-button variant="success" href="http://localhost:5173/#/paginacion">Juegos de mesa</b-button>
+    <b-button variant="success" href="http://localhost:5173/#/paginacion"
+      >Juegos de mesa</b-button
+    >
+
+    <div class="container mt-2">
+      <form @submit.prevent="submitForm">
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" v-model="formData.name" required />
+        <div
+          ref="container"
+          class="frc-captcha mt-2"
+          data-sitekey="FCMII3HVVENOU1DT"
+          data-lang="es"
+        ></div>
+        <button class="mt-2" type="submit">Enviar</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import { WidgetInstance } from "friendly-challenge";
+import { ref } from "vue";
+
 export default {
   data() {
     return {
+      container: ref("container"),
+      widget: ref(),
+      formData: {
+        name: "",
+        captchaToken: null,
+      },
       items: [
         {
           text: "Tienda de juguetes",
@@ -23,6 +48,31 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    submitForm: () => {},
+    doneCallback: (solution) => {
+      console.log("Captcha was solved. The form can be submitted.");
+      console.log(solution);
+    },
+    errorCallback: (err) => {
+      console.log("There was an error when trying to solve the Captcha.");
+      console.log(err);
+    },
+  },
+  mounted() {
+    if (this.$refs.container) {
+      this.widget = new WidgetInstance(this.$refs.container, {
+        startMode: "auto",
+        doneCallback: this.doneCallback,
+        errorCallback: this.errorCallback,
+      });
+    }
+  },
+  beforeDestroy() {
+    if (this.widget) {
+      this.widget.destroy();
+    }
   },
 };
 </script>
